5fbe0b4476644d1c04118dc26faf62ddfe42f631
taltara <taltarablus@gmail.com>
2020-06-07 11:48:58 +0300

    Crazy amount of changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,33 +4,35 @@ import Searcher from "../cmps/Searcher";
 import serviceTube from "../services/serviceTube";
 
 export class Home extends React.Component {
-  state = {
-    videos: null,
-  };
+    state = {
+        videos: null,
+    };
 
-  async componentDidMount() {
-    const videos = await serviceTube.query("Maroon");
+    async componentDidMount() {
+        const videos = await serviceTube.query("Maroon");
 
-    this.setState({ videos }, () => {
-      console.log(this.state);
-    });
-  }
-  componentDidUpdate() {}
+        this.setState({ videos }, () => {
+            console.log(this.state);
+        });
+    }
+    componentDidUpdate() { }
 
-  handleSubmit = async (searchValue) => {
-    const videos = await serviceTube.query(searchValue);
-    this.setState({ videos });
-  };
+    handleSubmit = async (searchValue) => {
+        const videos = await serviceTube.query(searchValue);
+        this.setState({ videos });
+    };
 
-  render() {
-    const { videos } = this.state;
+    render() {
+        const { videos } = this.state;
 
-    return (
-      <main className="main-home grid">
-        <Searcher handleSubmit={this.handleSubmit} />
-        {videos && <VideoList videos={videos} />}
-        <aside className="youtube-video"></aside>
-      </main>
-    );
-  }
+        return (
+            <main className="main-home flex column align-center">
+                <Searcher handleSubmit={this.handleSubmit} />
+                <section className="home-video-content flex align-start space-between">
+                    {videos && <VideoList videos={videos} />}
+                    <aside className="youtube-video"></aside>
+                </section>
+            </main>
+        );
+    }
 }
