2ffbf1f93cbc9a54cd969c29c8a2a9fc78c0e0c8
AbirNadav <pakakaw41@gmail.com>
2020-06-07 11:46:13 +0300

    123

--- src/cmps/VideoList.jsx
+++ src/cmps/VideoList.jsx
@@ -5,7 +5,7 @@ export function VideoList(props) {
   const { videos } = props;
 
   return (
-    <div className="video-list-container">
+    <div className="video-list-container flex align-center column">
       {videos.map((video, idx) => (
         <VideoPreview key={idx} video={video} />
       ))}
--- src/cmps/VideoPreview.jsx
+++ src/cmps/VideoPreview.jsx
@@ -3,14 +3,15 @@ import React from "react";
 export function VideoPreview(props) {
   const { video } = props;
   return (
-    <div className="video-preview-card flex column a-center j-center">
-      <h3 className="song-info">
-        {video.snippet.title ? video.snippet.title : "NO TITLE"}
-      </h3>
-
-      {/* <p>{video.snippet}</p> */}
+    <div className="video-preview-card flex ">
       <img alt="IMG OF VIDEO" src={video.snippet.thumbnails.medium.url}></img>
-      <span>{video.snippet.publishedAt}</span>
+      <div className="video-details-container flex align-start column space-between ">
+        <p className="song-info">
+          {video.snippet.title ? video.snippet.title : "NO TITLE"}
+        </p>
+
+        <span>{video.snippet.publishedAt}</span>
+      </div>
     </div>
   );
 }
