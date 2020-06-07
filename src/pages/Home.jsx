67a57be6b8a3cb52f7eec3d732813d73e02b1782
meshicohenn <meshicohenn@gmail.com>
2020-06-07 10:59:57 +0300

    youtube query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 // import { VideoList } from '../cmps/VideoList'
 // import { VideoPreview } from '../cmps/VideoPreview'
+import serviceTube from '../services/serviceTube'
 
 
 export class Home extends React.Component {
 
     state = {
-        videos: []
+        videos: null
     };
 
     componentDidMount() {
+        this.setState({videos:serviceTube.query('hello')},()=>{
+            console.log(this.state.videos);
+            
+        })
 
     }
     componentDidUpdate() {
@@ -22,7 +27,7 @@ export class Home extends React.Component {
 
         return (
             <main className="main-home grid">
-
+                
                 {/* <VideoList videos={videos} /> */}
 
             </main>
