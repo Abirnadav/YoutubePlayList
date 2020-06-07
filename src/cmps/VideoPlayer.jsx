d8a1eed1dcba158eef369f23a2a532282b38a1e6
shahar <shaharp2@gmail.com>
2020-06-07 12:05:44 +0300

    sf

--- src/cmps/VideoPlayer.jsx
+++ src/cmps/VideoPlayer.jsx
@@ -3,11 +3,16 @@ import React, { Component } from "react";
 export default class VideoPlayer extends Component {
   render() {
     return (
-      <iframe
-        width="420"
-        height="315"
-        src={`https://www.youtube.com/embed/${this.props.item.id.videoId}`}
-      ></iframe>
+      <>
+        <iframe
+          className="flex column"
+          width="420"
+          height="315"
+          src={`https://www.youtube.com/embed/${this.props.item.id.videoId}`}
+        ></iframe>
+
+        <h3>{this.props.item.snippet.title}</h3>
+      </>
     );
   }
 }
