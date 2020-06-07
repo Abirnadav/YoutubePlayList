d09bc2f7c4f93ef7e83dad0b7449a2613183d181
AbirNadav <pakakaw41@gmail.com>
2020-06-07 12:31:08 +0300

    !23

--- src/cmps/VideoPreview.jsx
+++ src/cmps/VideoPreview.jsx
@@ -3,13 +3,14 @@ import React from "react";
 export function VideoPreview(props) {
   const { video, setCurrVideo } = props;
   return (
-    <div
-      onClick={() => setCurrVideo(video)}
-      className="video-preview-card flex "
-    >
-      <img alt="IMG OF VIDEO" src={video.snippet.thumbnails.medium.url}></img>
+    <div className="video-preview-card flex ">
+      <img
+        onClick={() => setCurrVideo(video)}
+        alt="IMG OF VIDEO"
+        src={video.snippet.thumbnails.medium.url}
+      ></img>
       <div className="video-details-container flex align-start column space-between ">
-        <p className="song-info">
+        <p onClick={() => setCurrVideo(video)} className="song-info">
           {video.snippet.title ? video.snippet.title : "NO TITLE"}
         </p>
 
